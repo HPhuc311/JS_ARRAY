55262e348a0d60569116a7cbc01c150ad821a166
HoangPhuc <hoangphuc311tn@gmail.com>
2023-05-31 14:55:04 +0700

    first commit1

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function getElement(element){
   return document.querySelector(element)
 }
 
+//EX1: Nhập mảng 
+
 var arr = []
 var count = 0 
 
@@ -26,4 +28,45 @@ getElement('#clickToAdd').onclick= function(){
   getElement('#resultEx').innerHTML = outputArray()
 }
 
+// Ex2: Tính tổng các số dương
 
+function sumofPositive(){
+  var sum = 0
+  for( var i = 0; i < arr.length; i++){
+    if ( arr[i] > 0){
+        sum += arr[i]
+    }
+  }
+  document.querySelector('#resultEx1').innerHTML = ' Tổng số dương là: ' + sum
+}
+
+getElement('#clickResults1').onclick = sumofPositive
+
+
+// Ex2: Đếm các số dương
+
+function countofPositive(){
+  var dem = 0 
+  for (var j = 0; j < arr.length; j++){
+    if( arr[j] > 0){
+      dem++
+    } 
+  }
+
+  document.querySelector('#resultEx2').innerHTML = ' Số dương là: ' + dem
+}
+
+getElement('#clickResults2').onclick = countofPositive 
+
+
+// Ex3: Tìm số nhỏ nhất
+function smallestofNumber(){
+  var min = arr[0]
+  for(var i = 0; i < arr.length; i++)
+    if(arr[i] < min){
+      min = arr[i]
+    }
+    document.querySelector('#resultEx3').innerHTML = ' Số nhỏ nhất là: ' + min
+}
+
+getElement('#clickResults3').onclick = smallestofNumber
